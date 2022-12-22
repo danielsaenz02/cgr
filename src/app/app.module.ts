9ed47dbc15366854884d139f5e96a8f1bdcf0d76
Daniel S Saenz <Daniel.Saenz@co.ey.com>
2022-12-22 11:03:19 -0500

    CGR V1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MenubarModule} from 'primeng/menubar';
+import {InputTextModule} from 'primeng/inputtext';
+import {CarouselModule} from 'primeng/carousel';
+import { CarouselService } from './services/carousel/carousel.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,9 +15,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MenubarModule,
+    InputTextModule,
+    CarouselModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [CarouselService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
